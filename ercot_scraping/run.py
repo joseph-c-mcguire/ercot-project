9fb9e6a3f58e71448e48872173966b3ccbee713f
Joseph McGuire <bigmeanjoegreen@gmail.com>
2025-02-06 15:51:31 -0500

    Refactor import statements to use updated functions for fetching and storing settlement point prices

--- ercot_scraping/run.py
+++ ercot_scraping/run.py
@@ -1,12 +1,14 @@
 from ercot_scraping.ercot_api import (
-    fetch_dam_energy_bids,
+    fetch_settlement_point_prices,
     fetch_dam_energy_bid_awards,
+    fetch_dam_energy_bids,
     fetch_dam_energy_only_offers,
     fetch_dam_energy_only_offer_awards,
 )
 from ercot_scraping.store_data import (
-    store_bids_to_db,
+    store_prices_to_db,
     store_bid_awards_to_db,
+    store_bids_to_db,
     store_offers_to_db,
     store_offer_awards_to_db,
 )
